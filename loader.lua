5291684a47988d8d519e4fdd9b65c16a989b7d6c
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2024-05-28 17:51:57 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,9 @@
 if not isfile('OblivionAutofarm') then
     makefolder('OblivionAutoFarm')
-
-    writefile('OblivionAutoFarm/TimeStart.txt', 0)
-    writefile('OblivionAutoFarm/ProfitStart.txt', 0)
 end
 
 writefile('OblivionAutoFarm/TimeStart.txt', os.time())
 writefile('OblivionAutoFarm/ProfitStart.txt', nil)
+writefile('OblivionAutoFarm/Servers.txt', 0)
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/CorruptOblivion/Autofarm/main/script.lua'))()
