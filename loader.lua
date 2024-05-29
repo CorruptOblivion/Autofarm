8287a1671eee1be31cb0addb3e4c3dd4f18bb5e6
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2024-05-28 19:38:14 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,4 +6,9 @@ writefile('OblivionAutoFarm/TimeStart.txt', os.time())
 writefile('OblivionAutoFarm/ProfitStart.txt', 0)
 writefile('OblivionAutoFarm/Servers.txt', 0)
 
+local QueueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport) -- yes bro im aware that synapse is obsolete
+if QueueOnTeleport then
+	QueueOnTeleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/CorruptOblivion/Autofarm/main/script.lua'))()")
+end
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/CorruptOblivion/Autofarm/main/script.lua'))()
